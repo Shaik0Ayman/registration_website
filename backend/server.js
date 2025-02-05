2da147d7d281004d37079f3e5fc6eec79582889f
Shaik0Ayman <146252387+Shaik0Ayman@users.noreply.github.com>
2025-02-05 16:07:02 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Serve static files (CSS, JS, etc.) from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route to serve index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 // Handle form submission
 app.post('/register', (req, res) => {
     const formData = req.body;
